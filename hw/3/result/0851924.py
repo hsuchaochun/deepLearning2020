d0b9985f74dad6f0233a4f92cb48d418e79f519d
hsuchaochun <leohsu210200@gmail.com>
2020-12-28 15:57:43 +0800

    result analyse

--- hw/3/result/0851924.py
+++ hw/3/result/0851924.py
@@ -14,22 +14,22 @@ from tqdm import tqdm
 
 # Parse command line arguments
 argparser = argparse.ArgumentParser()
-argparser.add_argument('--filename', type=str, default="shakespeare_train.txt")
-argparser.add_argument('--model', type=str, default="rnn")
-argparser.add_argument('--n_epochs', type=int, default=500)
+argparser.add_argument('--filename', type=str, default="../dataset_Shakespeare/shakespeare_train.txt")
+argparser.add_argument('--model', type=str, default="lstm")
+argparser.add_argument('--n_epochs', type=int, default=2000)
 argparser.add_argument('--print_every', type=int, default=400)
 argparser.add_argument('--hidden_size', type=int, default=150)
 argparser.add_argument('--n_layers', type=int, default=2)
-argparser.add_argument('--learning_rate', type=float, default=0.05)
-argparser.add_argument('--chunk_len', type=int, default=50)
+argparser.add_argument('--learning_rate', type=float, default=0.001)
+argparser.add_argument('--seq_len', type=int, default=50)
 argparser.add_argument('--batch_size', type=int, default=100)
 argparser.add_argument('--shuffle', action='store_true')
 argparser.add_argument('--cuda', action='store_true')
-argparser.add_argument('--nth', type=int)
 args = argparser.parse_args()
 
 def read_file(filename):
     file = unidecode.unidecode(open(filename).read())
+    file = file.lower()
     '''vocab = set(file)
     vocab_to_int = {c:i for i,c in enumerate(vocab)}
     int_to_vocab = dict(enumerate(vocab))
@@ -37,7 +37,7 @@ def read_file(filename):
     return file, len(file)
 
 # Turning a string into a tensor
-def char_tensor(string):
+def char2tensor(string):
     tensor = torch.zeros(len(string)).long()
     for c in range(len(string)):
         try:
@@ -46,16 +46,9 @@ def char_tensor(string):
             continue
     return tensor
 
-# Readable time elapsed
-def time_since(since):
-    s = time.time() - since
-    m = math.floor(s / 60)
-    s -= m * 60
-    return '%dm %ds' % (m, s)
-
-def generate(decoder, prime_str='A', predict_len=500, temperature=0.8, cuda=False):
+def generate(decoder, prime_str='I', predict_len=500, temperature=0.8, cuda=False):
     hidden = decoder.init_hidden(1)
-    prime_input = Variable(char_tensor(prime_str).unsqueeze(0))
+    prime_input = Variable(char2tensor(prime_str).unsqueeze(0))
 
     if cuda:
         hidden = hidden.cuda()
@@ -78,23 +71,22 @@ def generate(decoder, prime_str='A', predict_len=500, temperature=0.8, cuda=Fals
         # Add predicted character to string and use as next input
         predicted_char = all_characters[top_i]
         predicted += predicted_char
-        inp = Variable(char_tensor(predicted_char).unsqueeze(0))
+        inp = Variable(char2tensor(predicted_char).unsqueeze(0))
         if cuda:
             inp = inp.cuda()
 
     return predicted
 
-
-def random_training_set(chunk_len, batch_size):
-    inp = torch.LongTensor(batch_size, chunk_len)
-    target = torch.LongTensor(batch_size, chunk_len)
+def random_training_set(seq_len, batch_size):
+    inp = torch.LongTensor(batch_size, seq_len)
+    target = torch.LongTensor(batch_size, seq_len)
     for bi in range(batch_size):
-        start_index = random.randint(0, file_len - chunk_len)
-        end_index = start_index + chunk_len + 1
+        start_index = random.randint(0, file_len - seq_len)
+        end_index = start_index + seq_len + 1
         
-        chunk = file[start_index:end_index]
-        inp[bi] = char_tensor(chunk[:-1])
-        target[bi] = char_tensor(chunk[1:])
+        seq = file[start_index:end_index]
+        inp[bi] = char2tensor(seq[:-1])
+        target[bi] = char2tensor(seq[1:])
     inp = Variable(inp)
     target = Variable(target)
     if args.cuda:
@@ -102,16 +94,16 @@ def random_training_set(chunk_len, batch_size):
         target = target.cuda()
     return inp, target
 
-def random_validation_set(chunk_len, batch_size):
-    inp = torch.LongTensor(batch_size, chunk_len)
-    target = torch.LongTensor(batch_size, chunk_len)
+def random_validation_set(seq_len, batch_size):
+    inp = torch.LongTensor(batch_size, seq_len)
+    target = torch.LongTensor(batch_size, seq_len)
     for bi in range(batch_size):
-        start_index = random.randint(0, val_file_len - chunk_len-1)
-        end_index = start_index + chunk_len + 1
+        start_index = random.randint(0, val_file_len - seq_len-1)
+        end_index = start_index + seq_len + 1
         
-        chunk = val_file[start_index:end_index]
-        inp[bi] = char_tensor(chunk[:-1])
-        target[bi] = char_tensor(chunk[1:])
+        seq = val_file[start_index:end_index]
+        inp[bi] = char2tensor(seq[:-1])
+        target[bi] = char2tensor(seq[1:])
     inp = Variable(inp)
     target = Variable(target)
     if args.cuda:
@@ -126,34 +118,37 @@ def train(inp, target):
     decoder.zero_grad()
     loss = 0
     acc=0
-    for c in range(args.chunk_len):
+    for c in range(args.seq_len):
         output, hidden = decoder(inp[:,c], hidden)
         loss += criterion(output.view(args.batch_size, -1), target[:,c])
             
     loss.backward()
     decoder_optimizer.step()
 
-    return loss.item() / args.chunk_len,hidden
-
+    return loss.item() / args.seq_len,hidden
 
 def validate(inp, target,hidden_inp):
     loss = 0
     hidden=hidden_inp
     acc=0
-    for c in range(args.chunk_len):
+    for c in range(args.seq_len):
         output, hidden = decoder(inp[:,c], hidden)
         loss += criterion(output.view(args.batch_size, -1), target[:,c])
         
-    return loss.item() / args.chunk_len
+    return loss.item() / args.seq_len
 
 def save():
-    save_filename = os.path.splitext(os.path.basename(args.filename))[0] + '.pt'
+    if args.model == "rnn":
+        save_filename = os.path.splitext(os.path.basename(args.filename))[0] + '.pt'
+    elif args.model == "lstm":
+        save_filename = os.path.splitext(os.path.basename(args.filename))[0] + '_lstm.pt'
+    
     torch.save(decoder, save_filename)
     print('Saved as %s' % save_filename)
 
-class CharRNN(nn.Module):
+class RNN_textGeneration(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, model="rnn", n_layers=1):
-        super(CharRNN, self).__init__()
+        super(RNN_textGeneration, self).__init__()
         self.model = model.lower()
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -190,10 +185,10 @@ all_characters = string.printable
 n_characters = len(all_characters)
 
 file, file_len = read_file(args.filename)
-val_file, val_file_len = read_file('shakespeare_valid.txt')
+val_file, val_file_len = read_file('../dataset_Shakespeare/shakespeare_valid.txt')
 
 # Initialize models and start training
-decoder = CharRNN(
+decoder = RNN_textGeneration(
     n_characters,
     args.hidden_size,
     n_characters,
@@ -207,41 +202,39 @@ if args.cuda:
     decoder.cuda()
 
 start = time.time()
-all_losses = []
+train_losses = []
 loss_avg = 0
 val_losses=[]
 
 try:
     print("Training for %d epochs..." % args.n_epochs)
     for epoch in range(1, args.n_epochs + 1):
-        loss,hidden_inp = train(*random_training_set(args.chunk_len, args.batch_size))
+        train_loss,hidden_inp = train(*random_training_set(args.seq_len, args.batch_size))
         
-        loss_avg += loss
-        all_losses.append(loss)
+        loss_avg += train_loss
+        train_losses.append(train_loss)
         
-
-        val_loss = validate(*random_validation_set(args.chunk_len, args.batch_size),hidden_inp=hidden_inp)
+        val_loss = validate(*random_validation_set(args.seq_len, args.batch_size),hidden_inp=hidden_inp)
         val_losses.append(val_loss)
         
-        if epoch % args.print_every == 0:
-            #print('[%s (%d %d%%) %.4f] length=%d val_loss=%.4f' % (time_since(start), epoch, epoch / args.n_epochs * 100, loss,len(all_losses),val_loss))
-            
-            out_res=generate(decoder, 'Wh', 100, cuda=args.cuda)
-            print(str(epoch)+":"+out_res)
+        if epoch % args.print_every == 0:            
+            out_res = generate(decoder, 'he', 100, cuda=args.cuda)
+            print(str(epoch) + ":\n" + out_res)
+            print('\ntrain_loss: ', train_loss, 'val_loss: ', val_loss, '\n')
 
     print("Saving...")
     save()
     
-    plt.title('3')
-    plt.plot(val_losses,color='red',label='Test Loss')
-    plt.plot(all_losses,color='Blue',label='Train Loss')
+    plt.title('training curve')
+    plt.plot(val_losses, color='Red', label='Test Loss')
+    plt.plot(train_losses, color='Blue', label='Train Loss')
     plt.ylabel('Loss')
     plt.legend()
     plt.show()
     
-    print('Hidden Size:'+str(args.hidden_size)+' Sequence length:'+str(args.chunk_len))
-    print(all_losses)
-    print(val_losses)
+    print('Hidden Size:' + str(args.hidden_size) + ' Sequence length:' + str(args.seq_len))
+    # print(train_losses)
+    # print(val_losses)
 
 except KeyboardInterrupt:
     print("Saving before quit...")
